fbf85da3dccaddfed512d9fda71433822c9bbf44
Ronald Wycoco <ronaldjvwycoco@gmail.com>
2023-04-02 20:00:30 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+{
+    let num1 = 31
+    let num2 = 2 
 
+    const multiply = num1 * num2;
+}
+
+{
+    let random = 23
+
+    Math.floor(Math.random()*10) + 1;
+}
+
+{
+    let num3 = 16
+    let num4 = 4
+
+    const mod = num3 / num4;
+}
+
+    let max = Math.max(10, 2, 1, 6, 18, 20)
 // Write your code here
